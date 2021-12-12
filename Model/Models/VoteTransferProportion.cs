34abdd4f420967e924ca88adb50d7888960617b3
Michael Hargreaves <contact@michaelrh04.co.uk>
2021-12-12 02:53:08 +0000

    Create new vote-transfer-proportion data structure

---
+++ Model/Models/VoteTransferProportion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public struct VoteTransferProportion
+    {
+        /// <summary>
+        /// The party from which voters will leave; the lesser party in any one constituency pact.
+        /// </summary>
+        string From { get; set; }
+        /// <summary>
+        /// The party to which new voters will turn; the greater party in any one constituency pact.
+        /// </summary>
+        string To { get; set; }
+        /// <summary>
+        /// The proportion of lesser party members willing to vote for the greater alliance member.
+        /// </summary>
+        double Will { get; set; }
+    }
+}
